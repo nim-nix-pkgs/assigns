127c0e93f1d37c54aa459bac5c3160cda4939625
hugosenari <hugosenari@gmail.com>
2022-03-12 00:34:03 -0300

    chore: re index assigns

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = ''syntax sugar for assignments'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."assigns-master".url = "path:./master";
+  inputs."assigns-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."assigns-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."assigns-v0_4_4".url = "path:./v0_4_4";
+  inputs."assigns-v0_4_4".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."assigns-v0_4_4".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."assigns-v0_5_1".url = "path:./v0_5_1";
+  inputs."assigns-v0_5_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."assigns-v0_5_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."assigns-v0_6_0".url = "path:./v0_6_0";
+  inputs."assigns-v0_6_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."assigns-v0_6_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ v0_4_4/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''syntax sugar for assignments'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-assigns-v0_4_4.flake = false;
+  inputs.src-assigns-v0_4_4.owner = "metagn";
+  inputs.src-assigns-v0_4_4.ref   = "refs/tags/v0.4.4";
+  inputs.src-assigns-v0_4_4.repo  = "assigns";
+  inputs.src-assigns-v0_4_4.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-assigns-v0_4_4"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-assigns-v0_4_4";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
